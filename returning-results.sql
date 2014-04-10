ba4ebe0f7aeb38c99ed9bbf694465b5dc8042e89
Michael Allen Smith <digitalcolony@gmail.com>
2014-04-09 18:07:23 -0700

    Create returning-results.sql

---
+++ returning-results.sql
@@ -0,0 +1,4 @@
+SELECT TOP 20 S.url, S.title, S.longDesc, S.quality, SUM(S.score) AS Score
+FROM #searchResults S
+GROUP BY S.url, S.title, S.longDesc, S.quality
+ORDER BY SUM(S.score) DESC, S.Quality DESC
